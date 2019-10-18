fa8752ed61eb8d1737a212c6431173f2ec86dcbe
birbaltamang <tamangbirbal23@gmail.com>
2019-10-18 17:14:59 +0545

    project created

---
+++ AppTransitions/AppTransitions/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.SharedTransitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTransitions
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1(string.Empty));
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var tappedItemData = e.Item as string;
+            //this is required in order to pass the views to animate
+            SharedTransitionNavigationPage.SetTransitionSelectedGroup(this, tappedItemData);
+            await Navigation.PushAsync(new Page1(tappedItemData));
+        }
+    }
+}
